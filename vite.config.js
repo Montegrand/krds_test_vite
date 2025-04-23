b61ecbafc5195ffca5803e7ed0faf3875a3b9551
Montegrand <hpt0311@naver.com>
2025-04-24 00:01:33 +0900

    .

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,20 @@ import react from '@vitejs/plugin-react';
 import AutoImport from 'unplugin-auto-import/vite';
 import path from 'path';
 
-export default defineConfig({
-  base: '/krds_test_vite/',
-  plugins: [
-    react(),
-    AutoImport({
-      imports: ['react'],
-      dts: true
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+export default defineConfig(({ mode }) => {
+    return {
+        base: mode === 'production' ? '/krds_test_vite/' : '/',
+        plugins: [
+            react(),
+            AutoImport({
+                imports: ['react'],
+                dts: true
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src')
+            }
+        }
     }
-  }
 });
